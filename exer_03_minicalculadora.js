31c3ea6359b9f327b3933630b5555efaf18f6f6c
Ferran <ferran.docencia@gmail.com>
2023-11-13 10:29:24 +0100

    Exercici calculadora acabat

--- exer_03_minicalculadora.js
+++ exer_03_minicalculadora.js
@@ -4,7 +4,54 @@
 // ** operaciones posibles:
 //      suma, resta, multiplicación y división
 
-calculadora(num1, operacion, num2)
+
+// calculadora(num1, operacion, num2)
 
 console.log(calculadora(1, "+", 4)); // 5
+console.log(calculadora(1, "-", 4)); // 5
+console.log(calculadora(1, "*", 4)); // 5
+console.log(calculadora(1, "/", 4)); // 5
+console.log(calculadora(1, "/", 0)); // 5
+console.log(calculadora(1, "%", 4)); // 5
 
+
+// función principal que organiza las
+// funciones con las operaciones
+function calculadora(num1, operacion, num2){
+    
+    switch (operacion) {
+
+        case "+":
+            return suma(num1, num2)
+        case "-":
+            return resta(num1, num2)
+        case "*":
+            return multiplicacion(num1, num2)
+        case "/":
+            return division(num1, num2)
+        case "%":
+            return num1 % num2
+        default:
+            return "Operación no identificada"
+          
+    }
+
+}
+
+
+// funciones con las operaciones específicas
+function suma(num1, num2){
+    return num1 + num2
+}
+function resta(num1, num2){
+    return num1 - num2
+}
+function multiplicacion(num1, num2){
+    return num1 * num2
+}
+function division(num1, num2){
+    if (num2 == 0) {
+        return "No se puede dividir por cero"
+    }
+    return num1 / num2
+}
