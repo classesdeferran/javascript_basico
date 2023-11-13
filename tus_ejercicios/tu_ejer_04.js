5a40d3d4a8d2889b6b9157f4830c8643f5b72e10
Ferran <ferran.docencia@gmail.com>
2023-11-13 11:25:48 +0100

    tus ejercicios: 1 solucionat

---
+++ tus_ejercicios/tu_ejer_04.js
@@ -0,0 +1,5 @@
+// CREAR UN PALÍNDROMO
+// Un palíndromo es un texto que tiene sentido
+// incluso si se escribe al revés.
+// En este ejercicio nuestro objetivo será invertir un
+// texto, como si se mirara en un espejo
---
+++ tus_ejercicios/tu_ejer_05.js
@@ -0,0 +1,2 @@
+// ¿ES UN PALÍNDROMO?
+// Quemos saber si un texto es el inverso de otro
