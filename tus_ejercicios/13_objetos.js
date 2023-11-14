716aa1342ea89fb57c16e1f374df31071a9ae662
Ferran <ferran.docencia@gmail.com>
2023-11-14 11:37:21 +0100

    Objetos

---
+++ tus_ejercicios/13_objetos.js
@@ -0,0 +1,29 @@
+let listaAlumnos = ["Peter", "Mary", "Clark"]
+
+// Necesito guardar información del:
+        // nombre
+        // apellido
+        // edad
+        // ciudad
+        // es_alumno_ahora
+
+listaAlumnosApellido = ["Parker", "Jane", "Kent"]
+
+let alumno1 = {nombre: "Peter", apellido: "Parker", edad: 25, ciudad: 'NY', es_alumno: true}
+
+console.log(typeof objetoAlumno)
+
+let alumno2 = new Object()
+alumno2.nombre = "Mary"
+alumno2["apellido"] = "Jane"
+alumno2.nombreCompleto = function () {
+    return this.nombre + " " + this.apellido
+}
+
+
+console.log(alumno2);
+
+console.log(alumno1.edad);
+
+console.log(`alumno2 es : ${alumno2.nombre} ${alumno2.apellido}`);
+console.log(`alumno es ${alumno2.nombreCompleto()}`);
